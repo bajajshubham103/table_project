32ea37ecccb565340a4aa1770fba458d6066d0b1
bajajshubham103 <77060853+bajajshubham103@users.noreply.github.com>
2021-01-06 19:34:05 +0530

    Add files via upload

---
+++ table_proagram.py
@@ -0,0 +1,24 @@
+#table proagram
+a       = input('Enter table ')
+a       = int(a)
+two     =   2
+three	=	3
+four	=	4
+five	=	5
+six	    =	6
+seven	=	7
+eight	=	8
+nine	=	9
+ten 	=	10
+print   (a, 'ones are ', a)  
+print   (a, 'twos are ', a * two)
+print   (a, 'threes are ', a * three)
+print   (a, 'fours are ', a * four)
+print   (a, 'fives are ', a * five)
+print   (a, 'sixes are ', a * six)
+print   (a, 'sevens are ', a * seven)
+print   (a, 'eights are ', a * eight)
+print   (a, 'nines are ', a * nine)
+print   (a, 'tens are ', a * ten)
+
+input   (" press enter to exit ")
